7d617b1a350381f1e8b445c11b23346816264f0f
Ian Burgmyer <spectere@gmail.com>
2018-05-16 16:49:40 -0400

    Here we go again!

---
+++ Threads.Player/Program.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+namespace Threads.Player {
+    internal static class Program {
+        private static void Main(string[] args) {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
